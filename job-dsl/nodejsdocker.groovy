7a0b6f079ad9d2be9261db0ad843b1d25f6b098e
kirtidevkota <88785545+kirtidevkota@users.noreply.github.com>
2021-08-23 12:19:54 -0500

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -3,7 +3,7 @@ job('NodeJS Docker example') {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
-      
+        }
     }
     triggers {
         scm('H/5 * * * *')
